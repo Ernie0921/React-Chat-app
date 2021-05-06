6df4b812eaa101daddc78947ea1aef9eae6c7a58
Ernesto Carrillo <ernesto.carrillo10@icloud.com>
2021-05-06 01:13:29 -0400

    commpleted the theirmessages page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ChatFeed from './components/ChatFeed';
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height= "100vh"   //how you want the chat to appear in the broswer
+            projectID= "478b2fc3-dd71-4499-8c03-1050e710122e"    //a string once you create chat application 
+            userName="CaptainAmerica"         // name of user 
+            userSecret="1234" // password of user 
+            renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps} />}
+        />
+    )
 }
 
 export default App;
